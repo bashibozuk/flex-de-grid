ff21a05a5d948039ad653efc02cd3efd6467f802
Milen Hristov <milen.schmasterz@gmail.co>
2019-07-27 15:34:18 +0300

    Added bower.json, LICENSE, README.md. Changed gulpfile.js

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-TBD
+# Flexbox
+
+CSS framework for building layouts using CSS flex API
+## Installation
+
+Install via npm:
+
+```
+$ npm install --save flex-grid
+```
+
+Install via bower:
+
+```
+$ bower install --save flex-grid
+```
+
+## Usage
+```
+//TODO
+```
+
+## License
+
+MIT
+
+## Contributing
+
+1. Fork it
+2. Create your feature branch (`git checkout -b my-fancy-feature`)
+3. Commit your changes (`git commit -am 'Add some fancy feature'`)
+4. Run gulp task (`gulp css`)
+5. Push to the branch (`git push origin my-fancy-feature`)
+6. Create new Pull Request
--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,51 @@
-var gulp    = require('gulp'),
-    sass    = require('gulp-sass'),
-    rename  = require('gulp-rename'),
-    cssmin  = require('gulp-minify-css');
+(() => {
 
-gulp.task('scss', function() {
-    return gulp.src('scss/*.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('css'))
-        .pipe(cssmin())
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(gulp.dest('css'));
-});
+    'use strict';
 
-gulp.task('watch', function() {
-    gulp.watch('scss/*.scss', ['scss']);
-});
+    /**************** gulpfile.js configuration ****************/
 
-gulp.task('default', ['scss', 'watch']);
+    const
+
+        // development or production
+        devBuild  = ((process.env.NODE_ENV || 'development').trim().toLowerCase() === 'development'),
+
+        // directory locations
+        dir = {
+            src         : 'scss/',
+            build       : 'css/'
+        },
+
+        // modules
+        gulp          = require('gulp'),
+        sass          = require('gulp-sass'),
+        rename       = require('gulp-rename'),
+        cssmin  = require('gulp-minify-css');
+
+
+    console.log('Gulp', devBuild ? 'development' : 'production', 'build');
+    const cssConfig = {
+
+        src         : dir.src + 'flex-grid.scss',
+        watch       : dir.src + '/*',
+        build       : dir.build + '',
+        sassOpts: {
+            sourceMap       : devBuild,
+            precision       : 3,
+            errLogToConsole : true
+        },
+
+    };
+
+    function css() {
+
+        return gulp.src(cssConfig.src)
+            .pipe(sass(cssConfig.sassOpts).on('error', sass.logError))
+            .pipe(gulp.dest(cssConfig.build))
+            .pipe(cssmin())
+            .pipe(rename({ suffix: '.min' }))
+            .pipe(gulp.dest('css'));
+
+    }
+    exports.css = gulp.series(css);
+
+})();
